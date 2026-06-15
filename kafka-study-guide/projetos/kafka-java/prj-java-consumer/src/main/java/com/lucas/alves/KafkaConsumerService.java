0724e97a61cce999a11397b2faba385d715191a2
Lucas Alves <olalvezluc@gmail.com>
2026-06-15 19:54:50 -0300

    Passa a usar a anotação KafkaListner do spring kafka para simplificar

--- kafka-study-guide/projetos/kafka-java/prj-java-consumer/src/main/java/com/lucas/alves/KafkaConsumerService.java
+++ kafka-study-guide/projetos/kafka-java/prj-java-consumer/src/main/java/com/lucas/alves/KafkaConsumerService.java
@@ -1,48 +1,20 @@
 package com.lucas.alves;
 
-import java.time.Duration;
-import java.util.Arrays;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 
 @Service
 public class KafkaConsumerService {
     private final KafkaConsumer<String, String> consumer;
     private static final String TOPIC = "mensagens";
 
-    private volatile boolean running = true;
-
-    public KafkaConsumerService(KafkaConsumer<String, String> consumer) {
+    public KafkaConsumerService(KafkaConsumer consumer) {
         this.consumer = consumer;
-        consumer.subscribe(Arrays.asList(TOPIC));
     }
 
-    @PostConstruct
-    public void start() {
-        Thread consumerThread = new Thread(this::read);
-        consumerThread.setName("kafka-consumer-thread");
-        consumerThread.start();
-    }
-
-    @PreDestroy
-    public void stop() {
-        running = false;
-        consumer.wakeup(); // interrompe um poll() bloqueado
-        consumer.close();
-    }
-
-    public void read() {
-        while(running){
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-            for (ConsumerRecord<String, String> record : records) {
-                System.out.println("Mensagem Lida: " + record.value());
-            }
-        }
+    @KafkaListener(topics = TOPIC, groupId = "java-consumer-group")
+    public void read(String mensagem) {
+        System.out.println("Mensagem Lida: [" + mensagem + "]");
     }
 }
