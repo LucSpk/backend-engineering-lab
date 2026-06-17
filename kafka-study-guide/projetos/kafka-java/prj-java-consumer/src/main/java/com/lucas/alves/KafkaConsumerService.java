b2b40f5e9c1b0675c16804cd95d66eb13dd06b45
Lucas Alves <olalvezluc@gmail.com>
2026-06-17 19:44:13 -0300

    Refactor Kafka consumer setup by removing KafkaConsumerConfig and updating KafkaConsumerService for simplified message handling

--- kafka-study-guide/projetos/kafka-java/prj-java-consumer/src/main/java/com/lucas/alves/KafkaConsumerService.java
+++ kafka-study-guide/projetos/kafka-java/prj-java-consumer/src/main/java/com/lucas/alves/KafkaConsumerService.java
@@ -1,20 +1,18 @@
 package com.lucas.alves;
 
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerService {
-    private final KafkaConsumer<String, String> consumer;
     private static final String TOPIC = "mensagens";
 
-    public KafkaConsumerService(KafkaConsumer consumer) {
-        this.consumer = consumer;
-    }
-
-    @KafkaListener(topics = TOPIC, groupId = "java-consumer-group")
+    @KafkaListener(topics = TOPIC)
     public void read(String mensagem) {
-        System.out.println("Mensagem Lida: [" + mensagem + "]");
+        try {
+            System.out.println("Mensagem Lida: [" + mensagem + "]");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
