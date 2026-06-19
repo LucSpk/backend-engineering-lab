ceb1f502fde2134393aa2976827d57148ebef4d4
Lucas Alves <olalvezluc@gmail.com>
2026-06-18 21:13:16 -0300

    Add jSensors dependency and refactor KafkaProducerService to send structured temperature messages

--- kafka-study-guide/projetos/kafka-java/prj-java-producer/src/main/java/com/lucas/alves/KafkaProducerService.java
+++ kafka-study-guide/projetos/kafka-java/prj-java-producer/src/main/java/com/lucas/alves/KafkaProducerService.java
@@ -1,29 +1,60 @@
 package com.lucas.alves;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Random;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 @Service
 public class KafkaProducerService {
     private final KafkaProducer<String, String> producer;
     private static final String TOPIC = "mensagens";
-
-    private int num = 0;
     
     public KafkaProducerService() {
         this.producer = KafkaProducerConfig.create();
     }
 
-    public void send(String mensagem) {
-        System.out.println("Enviando a mensagem: [" + mensagem + "]...");
-        producer.send(new ProducerRecord<>(TOPIC, mensagem));
+    public void send(Temperatura mensagem) {
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println("Envia mensagem: " + mensagem);
+        try {
+            String json = mapper.writeValueAsString(mensagem);
+            producer.send(new ProducerRecord<>(TOPIC, json));
+        } catch(Exception ex) {
+            System.out.println("Mensagem não enviada!");
+        }
     }
 
     @Scheduled(fixedDelay = 5000)
     private void enviaMenagem() {
-        send("Mensagem " + num);
-        num ++;
+        send(this.getLeitura());
+    }
+
+    private Temperatura getLeitura() {
+        Double temp = 27d;
+
+        temp += new Random().nextDouble(1.5);
+        temp -= new Random().nextDouble(1.5);
+
+        return new Temperatura("Celcius (C°)", String.format("%.2f", temp), LocalDateTime.now().toString());
+    }
+
+}
+
+record Temperatura(String unidade, String temp, String timeStamp) {
+    @Override
+    public final String toString() {
+        return timeStamp + " - " + temp + " " + unidade;
     }
 }
